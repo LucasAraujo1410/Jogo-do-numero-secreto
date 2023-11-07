781c0b92b538cd5846975934caadfc0822c41095
Lucas Araujo <lucasaraujo.exe@hotmail.com>
2023-11-07 11:01:28 -0300

    Envio da correção onde nãoi se especifica o parágrafo

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function exibirTextoNaTela(tag,texto){
 }
 function exibeMensagemInicial(){
     exibirTextoNaTela('h1','SECRET NUMBER GAME');
-    
+    exibirTextoNaTela('p','Digite um número de 1 a 10');//CHAMNDO A FUNÇÃO
 }
 exibeMensagemInicial();
 
